8efd31dd8f08abe67ddae25ba573f8fb27909ebf
Bourhano <bourhan.dernayka@gmail.com>
2020-09-08 17:14:21 +0000

    rebuild solution

--- 0x00-singly_linked_lists/101-hello_holberton.asm
+++ 0x00-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,17 @@
-		  global    main
+section .rodata
+format:	 db 'Hello %s', 10, 0
+name:	   db 'Holberton', 0
 
-	          section   .text
-		  extern    printf
-main:	   	  mov       esi, message
-	          mov       edi, fmt
-	          mov       eax, 0	 ;printf is varargs, so EAX counts # of non-integer arguments being passed
-		  call printf
-		  mov ebx, 0      	 ; normal-exit code
-	          mov eax, 1      	 ; process-termination service
-
-	          section   .data
-message:	  db        "Hello, Holberton", 10 ; note the newline at the end
-fmt:		  db 	    "%s", 0
+	section .text
+	        global main
+	        extern printf
+main:
+	;;  printf(format, name)
+	        mov rdi, format
+	        mov rsi, name
+	;;  no XMM registers
+		mov rax, 0
+	        call printf
+	;;  return 0
+	        mov rax, 0
+	        ret
