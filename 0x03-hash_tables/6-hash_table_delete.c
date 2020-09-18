23687c60ec372770596f8185874587c3d7bae6ff
Bourhano <bourhan.dernayka@gmail.com>
2020-09-18 21:08:21 +0000

    remove unused variable

--- 0x03-hash_tables/6-hash_table_delete.c
+++ 0x03-hash_tables/6-hash_table_delete.c
@@ -2,20 +2,35 @@
 #include <stdio.h>
 
 /**
+ * free_list - Frees a linked list
+ * @head: ptr to the head of the list
+ **/
+void free_list(hash_node_t *head)
+{
+	if (head == 0)
+		return;
+	if (head->next)
+		free_list(head->next);
+	free(head->value);
+	free(head->key);
+	free(head);
+}
+
+/**
  * hash_table_delete - deletes a hash table
  * @ht: pointer to the hash table
  **/
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *hn;
-	unsigned long int idx = 0, first = 1;
+	unsigned long int idx = 0;
 
 	if (ht == 0)
 		return;
 	while (idx < ht->size)
 	{
 		hn = ht->array[idx];
-		free(hn);
+		free_list(hn);
 		idx++;
 	}
 	free(ht);
