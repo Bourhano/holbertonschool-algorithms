5243e37998d50208e14f2058b7ab4b5849f8c6df
Bourhano <bourhan.dernayka@gmail.com>
2020-10-23 20:47:31 +0000

    initialize directory

---
+++ 0x05-pathfinding/pathfinding.h
@@ -0,0 +1,9 @@
+#ifndef PATHFINDING_H
+#define PATHFINDING_H
+#include<stdio.h>
+#include "queues.h"
+#include "graphs.h"
+queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target);
+queue_t *backtracking_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start, vertex_t const *target);
+#endif
