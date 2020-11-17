d764c6515be0abc97c8ea8fb9824178bcd838eaf
Bourhano <bourhan.dernayka@gmail.com>
2020-11-17 21:16:10 +0000

    fix return type

--- 0x06-nary_trees/2-nary_tree_traverse.c
+++ 0x06-nary_trees/2-nary_tree_traverse.c
@@ -1,11 +1,13 @@
 #include "nary_trees.h"
 
 /**
- * nary_tree_traverse - DeletesWalks the whole N-ary tree, node by node.
+ * nary_tree_traverse - Walks the whole N-ary tree, node by node.
  * @root: pointer to the root of the tree
  * @action: a function pointer to be ran on each node
+ *
+ * Return: maximum depth
  **/
-void nary_tree_traverse(nary_tree_t *root,
+size_t nary_tree_traverse(nary_tree_t const *root,
 			void (*action)(nary_tree_t const *node, size_t depth))
 {
 	static size_t depth, max_depth;
